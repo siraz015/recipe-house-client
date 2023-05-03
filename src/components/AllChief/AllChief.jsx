4187d672e1cf63384d36622c32d890e1c71d573d
siraz015 <siraj015213@gmail.com>
2023-05-03 22:08:17 +0600

    implement chief card

---
+++ src/components/AllChief/AllChief.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Chief from '../Chief/Chief';
+
+const AllChief = () => {
+    const [chefs, setChefs] = useState([]);
+    console.log('chefs', chefs);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/allchief')
+            .then(res => res.json())
+            .then(data => setChefs(data))
+    }, [])
+
+    return (
+        <div className='chefContainer container p-10 grid grid-cols-3'>
+            {
+                chefs.map(chief => <Chief
+                    key={chief.id}
+                    chief={chief}
+                    ></Chief>)
+            }
+        </div>
+    );
+};
+
+export default AllChief;
+
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import AllChief from '../AllChief/AllChief';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <AllChief></AllChief>
         </div>
     );
 };
